b31145b4a58243b41c7aa1617bcc435c41622698
solar <1520905635@qq.com>
2023-05-14 22:26:28 +0800

    项目第一次全部push

--- src/main/java/com/example/gitdemo/study.java
+++ src/main/java/com/example/gitdemo/study.java
@@ -3,5 +3,6 @@ package com.example.gitdemo;
 public class study {
     public static void main(String[] args) {
         System.out.println("hello,world");
+        System.out.println("终于学会了git的大部分操作");
     }
 }
